6362a8581f98c62f2f77623d113bf097b0bdad17
Harini A <hari628122004@gmail.com>
2025-01-21 20:56:03 +0530

    Create to lower case - 709 .py
    For the below problem here is a python code 
    
    Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
    
    Example 1:
    Input: s = "Hello"
    Output: "hello"
    
    Example 2:
    Input: s = "here"
    Output: "here"
    
    Example 3:
    Input: s = "LOVELY"
    Output: "lovely"

---
+++ to lower case - 709 .py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def toLowerCase(self, s):
+        res = s.lower()
+        return res
+obj = Solution()
+obj.toLowerCase("Hello")
+obj.toLowerCase("here")
+obj.toLowerCase("LOVELY")
