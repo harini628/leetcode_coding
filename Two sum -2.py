6303abe984d10a49b14a69a33c28f9c0cabb3dd1
Harini A <hari628122004@gmail.com>
2025-02-04 22:43:39 +0530

    Create Two sum -2.py
    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
    
    You may assume that each input would have exactly one solution, and you may not use the same element twice.
    
    You can return the answer in any order.
    
     
    
    Example 1:
    
    Input: nums = [2,7,11,15], target = 9
    Output: [0,1]
    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
    Example 2:
    
    Input: nums = [3,2,4], target = 6
    Output: [1,2]
    Example 3:
    
    Input: nums = [3,3], target = 6
    Output: [0,1]

---
+++ Two sum -2.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        add = 0
+        arr = []
+        for i in range(len(nums)-1):
+            for j in range(1,len(nums)):
+                add = nums[i] + nums [j]
+                if(target == add):
+                    arr.append(i)
+                    arr.append(j)
+                    break
+        return arr   
+            
+obj = Solution()
+obj.twoSum([2,7,11,15],9)
+obj.twoSum([3,2,4],6)
+obj.twoSum([3,3],6)
