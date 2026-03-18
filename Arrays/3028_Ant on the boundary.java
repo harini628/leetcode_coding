3d1ca3600aeb9ade9f2fd3470c1d5f7585529c9d
Harini A <hari628122004@gmail.com>
2026-03-19 00:43:05 +0530

    Add Solution class for boundary count calculation

---
+++ Arrays/3028_Ant on the boundary.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int returnToBoundaryCount(int[] nums) {
+        int sum = 0;
+        int count = 0;
+        for(int i : nums){
+            sum += i;
+            if(sum ==0)count++;
+        }
+        return count;
+    }
+}
