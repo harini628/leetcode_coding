1c0b2e785e8ba9ba15cd6d4bcb1173b53b59ac21
Harini A <hari628122004@gmail.com>
2026-04-19 01:55:12 +0530

    Implement mirrorDistance method to calculate distance

---
+++ Number Math/3783_Mirror Distance of an Integer.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int mirrorDistance(int n) {
+        int result = reverseNum(n) - n;
+        if(result < 0){
+            result = result * -1;
+        }
+        return result;
+    }
+    public static int reverseNum(int n){
+        int result = 0;
+        while(n!=0){
+            int rem = n%10;
+            result = result * 10 + rem;
+            n = n/10;
+        }
+        return result;
+    }
+}
